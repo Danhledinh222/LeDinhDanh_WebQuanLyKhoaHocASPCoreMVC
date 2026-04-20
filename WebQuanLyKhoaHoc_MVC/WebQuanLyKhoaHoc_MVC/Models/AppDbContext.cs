d47576d32b6f683f506e49cab44528362cf7e9ba
Lê Đình Danh <danhledinh2607@gmail.com>
2026-04-20 13:08:06 +0700

    3.tao AppDbcontext

---
+++ WebQuanLyKhoaHoc_MVC/WebQuanLyKhoaHoc_MVC/Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace learnMVC.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseClass> CourseClasses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
