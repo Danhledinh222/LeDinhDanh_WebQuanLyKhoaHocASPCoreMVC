c3bcaaa7888de2e2258b376c7686e2d1b013eb54
Lê Đình Danh <danhledinh2607@gmail.com>
2026-04-20 13:04:34 +0700

    1.Tao project va cai cac enityFrameWork

---
+++ WebQuanLyKhoaHoc_MVC/WebQuanLyKhoaHoc_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebQuanLyKhoaHoc_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
