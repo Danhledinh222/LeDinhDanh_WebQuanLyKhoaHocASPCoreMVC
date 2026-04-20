729805a63321507fbd11f95b0be388aa1df345b7
Lê Đình Danh <danhledinh2607@gmail.com>
2026-04-20 13:11:45 +0700

    4.cau hinh appsettings.json,Program va sinh code Migration tao Database

--- WebQuanLyKhoaHoc_MVC/WebQuanLyKhoaHoc_MVC/Program.cs
+++ WebQuanLyKhoaHoc_MVC/WebQuanLyKhoaHoc_MVC/Program.cs
@@ -1,8 +1,12 @@
+using learnMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
